acd59cf35c8b9a9679027c299e65bfa8cb7d94dc
EricMichaelSmith <ericmsmith@gmail.com>
2014-02-16 19:13:55 -0500

    create_sample_distribution work

---
+++ SubwayOptimization/create_sample_distribution.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 16 18:42:03 2014
+
+@author: Eric
+
+2014-02-16: okay, you have the Gaussian cloud, but add some noise to it!
+"""
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+def create_sample_distribution(plotDistributionL = True):
+    """Creates a simple distribution comprised of three Gaussian-noise, Gaussian-shaped probability clouds with a Gaussian-noise background."""
+
+    numXPoints = 128
+    numYPoints = 128
+    distributionM = abs(np.random.randn(numXPoints, numYPoints))
+    
+    # Add background noise
+    cloudCenter2C = np.array([[30, 15],
+                              [20, 30],
+                              [100, 100]])
+    cloudMagnitudeC = np.array([10, 7, 15])
+    cloudWidthC = np.array([7, 7, 7])
+    
+    # Add Gaussian clouds with Gaussian noise
+    numClouds = cloudCenter2C.shape[0]
+    for lCloud in range(numClouds):
+        additionalDistributionM = np.array(np.empty(distributionM.shape))
+        additionalDistributionM.fill(np.nan)
+        # Create empty array containing the amount by which this cloud adds onto the existing distribution
+        
+        for lXPoint in range(numXPoints):
+            for lYPoint in range(numYPoints):
+                
+                # Setting up the exponential
+                squaredDistance = \
+                    math.sqrt((lXPoint-cloudCenter2C[lCloud, 0])**2 +
+                    (lYPoint-cloudCenter2C[lCloud, 1])**2)
+                sigma = cloudWidthC[lCloud]
+                magnitude = cloudMagnitudeC[lCloud]
+                additionalDistributionM[lXPoint, lYPoint] = \
+                    magnitude/(sigma*math.sqrt(2*math.pi)) * \
+                    math.exp(-squaredDistance/(2*sigma**2))
+                    
+        distributionM += additionalDistributionM
+        
+    if plotDistributionL:
+        plot_sample_distribution(distributionM)
+        
+    return distributionM
+    
+    
+    
+def plot_sample_distribution(distributionM):
+    plt.imshow(distributionM)
+    
+    
+
+if __name__ == "__main__":
+    create_sample_distribution(1)
--- SubwayOptimization/find_curves_prototype.py
+++ SubwayOptimization/find_curves_prototype.py
@@ -3,16 +3,16 @@
 Created on Sun Feb 16 12:39:04 2014
 
 @author: Eric Smith
+
+2014-02-16: I suppose I import this other file?
 """
 
-import numpy as np
-from scipy.cluster.vq import *
 
 
 def find_curves_prototype():
     """Run entire prototype of generating a population distribution, fitting curves to it, and plotting the output."""    
 
-#   Define sample population distribution.
+initialDistributionM = create_sample_distribution()
 #   Create weight matrix from the distribution.
 #   For each curve:
 #       Find best-fit curve through logistic regression.
@@ -21,25 +21,3 @@ def find_curves_prototype():
 #   Plot sample distribution.
 #   Plot curves on top of distribution.
 #   On separate figure, plot remainder of weight matrix.
-
-
-
-def create_sample_distribution():
-    distributionM = abs(np.random.randn(128, 128))
-    
-    cloudCenter2C = np.array([[30, 15],
-                              [20, 30],
-                              [100, 100]])
-    cloudMagnitudeC = np.array([10, 7, 15])
-    
-    
-    numClouds = shape(cloudCenter2C)[0]
-    numXPoints = shape(distM)[0]
-    numYPoints = shape(distM)[1]
-    for lCloud in range(numClouds):
-        distanceM = np.array(shape(distributionM))
-        # Create empty distance array
-        
-        for lXPoint in range(numXPoints):
-            for lYPoint in range(numYPoints):
-                
