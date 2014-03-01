356bda64d2d920fa8cb8fe887570f99f96ca234e
EricMichaelSmith <ericmsmith@gmail.com>
2014-03-01 15:48:38 -0500

    Work on reading in unemployment stats

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Ideas
 Testing repository for various projects.
 
 SubwayOptimization: Attempt to find an optimum subway line arrangement given a density map and a weighting metric
+
+VotingEconomyCorrelation: Seeing if there is a correlation on the US county level between unemployment changes and presidential voting shifts between 2008 and 2012
--- VotingEconomyCorrelation/main.py
+++ VotingEconomyCorrelation/main.py
@@ -5,16 +5,26 @@ Created on Fri Feb 28 07:56:38 2014
 @author: Eric Smith
 
 Determines whether a correlation exists between 2008/2012 voting shifts and unemployment shifts
+
+2014-03-01: See http://docs.scipy.org/doc/numpy/user/basics.io.genfromtxt.html
 """
 
+import numpy as np
 import os
 
 
+
 def main():
+    
+    # Demo
+    fileNameS = 'laucnty08.txt'
+    pathS = r'C:\E\GitHub\Computing\Ideas\VotingEconomyCorrelation\unemployment_statistics'
+    tableM = read_unemployment_file(pathS, fileNameS)
+    return tableM
   
   ## Load data: FIPS code, state name, county name, shape data, percentage voting for Obama in 2008 and 2012, percentage voting for McCain/Romney in 2008 and 2012, unemployment data per county for 2008, 2009, 2010, 2011, 2012
   
-  # Load election 
+  # Load election data
   # {{{}}}
   
   # Transform all of that data into a usable form
@@ -30,8 +40,8 @@ def main():
   # {{{}}}
   
   
-  
-  def read_unemployment_file(pathS, fileNameS):
-    with open(os.path.join(pathS, fileNameS)) as file:
-      values = file.read()
-      # {{{But split values on \t, right?}}}
+
+def read_unemployment_file(pathS, fileNameS):
+    tableM = np.genfromtxt(os.path.join(pathS, fileNameS), skip_header=6, skip_footer=3210)
+    # {{{other stuff. Fix fields that didn't import correctly and obviously don't skip so many lines at the end.}}}
+    return tableM
