637329277dd4587eee05dd6e9fce7492fc29c184
Eric Smith <ericmsmith@gmail.com>
2014-03-05 08:27:15 -0500

    Using pandas instead of numpy for csv read-in

--- VotingEconomyCorrelation/read_election_2012_file.py
+++ VotingEconomyCorrelation/read_election_2012_file.py
@@ -9,22 +9,29 @@ Reads in 2012 US election results, from http://www.theguardian.com/news/datablog
 import config
 import numpy as np
 import os
+from pandas import Series, DataFrame
+import pandas as pd
 
 
 
 def main():
+  
+    # Read in 2012 election data
     filePathS = os.path.join(config.basePathS, "election_statistics",
                              "US_elect_county__2012.csv")
-                              
-    fullTableRA = np.recfromcsv(filePathS,
-                              delimiter=',',
-                              case_sensitive=True,
-                              deletechars='',
-                              replace_space='_')
+    fullTableDF = pd.read_table(filePathS)
+#    fullTableRA = np.recfromcsv(filePathS,
+#                              delimiter=',',
+#                              case_sensitive=True,
+#                              deletechars='',
+#                              replace_space='_')
                               
                               
     # Remove entries that correspond to the voting records of the entire state
-    validRowsLC = fullTableRA.FIPS_Code.astype(bool)
+    validRowsLC = fullTableDF.FIPS_Code.astype(bool)
+    
+    # Extract the important fields for each row
+    
     
     # {{{get the candidates to be in the right order and create a subtable with only the rows and fields you need}}}
     
@@ -33,20 +40,20 @@ def main():
     
     
     
-def extract_votes(fullTableRA, iRow):
-  numDemVotes = run_extract_votes_loop(fullTableRA, iRow, "Dem")
-  numGOPVotes = run_extract_votes_loop(fullTableRA, iRow, "GOP")
+def extract_votes(fullTableDF, iRow):
+  numDemVotes = run_extract_votes_loop(fullTableDF, iRow, "Dem")
+  numGOPVotes = run_extract_votes_loop(fullTableDF, iRow, "GOP")
   
   return (numDemVotes, numGOPVotes)
   
   
   
-def run_extract_votes_loop(fullTableRA, iRow, partyS):
-  if fullTableRA.Party[iRow] == partyS:
-    return fullTableRA.Votes[iRow]
+def run_extract_votes_loop(fullTableDF, iRow, partyS):
+  if fullTableDF.Party[iRow] == partyS:
+    return fullTableDF.Votes[iRow]
   else:
     iParty = 0
     while True:
       iParty += 1
-      if fullTableRA("Party_" + str(iParty)) == partyS:
-        return fullTableRA("Votes_" + str(iParty))[iRow]
+      if fullTableDF["Party_" + str(iParty)] == partyS:
+        return fullTableDF["Votes_" + str(iParty)][iRow]
