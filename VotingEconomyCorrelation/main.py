e0186d6261ea5dbc0060858dec2fc416548401b6
Eric Smith <ericmsmith@gmail.com>
2014-03-25 08:37:08 -0400

    Trying to assign colors in the DataFrame

--- VotingEconomyCorrelation/main.py
+++ VotingEconomyCorrelation/main.py
@@ -6,10 +6,14 @@ Created on Fri Feb 28 07:56:38 2014
 
 Determines whether a correlation exists between 2008/2012 voting shifts and unemployment shifts
 
-2014-03-23: Take care of all of the idiosyncrasies in the data: missing values, misaligned counties, etc. Probably just delete all of the Alaska data.
+2014-03-25: Assign color values to the DataFrame properly and finish the test of plotting election 2008 data from fullDF. Once that's done, see which counties aren't being plotted and take care of all of the idiosyncrasies in the data: missing values, misaligned counties, etc. Probably just delete all of the Alaska data. Do you want to eventually change all of the inner joins back to outer joins, or will it not matter in the end?
 """
 
+from matplotlib.collections import PatchCollection
+import matplotlib.pyplot as plt
+import numpy as np
 import os
+import pdb
 
 import config
 reload(config)
@@ -47,13 +51,36 @@ def main():
 #    election2008.plot_county_results
     
     # Merging DataFrames
-    fullDF = fipsDF.join(election2008_DF, how='outer')
-    fullDF = fullDF.join(election2012_DF, how='outer')
+#    fullDF = fipsDF.join(election2008_DF, how='outer')
+#    fullDF = fullDF.join(election2012_DF, how='outer')
+#    for unemploymentDF in unemploymentDF_L:
+#        fullDF = fullDF.join(unemploymentDF, how='outer')
+    fullDF = fipsDF.join(election2008_DF, how='inner')
+    fullDF = fullDF.join(election2012_DF, how='inner')
     for unemploymentDF in unemploymentDF_L:
-        fullDF = fullDF.join(unemploymentDF, how='outer')
+        fullDF = fullDF.join(unemploymentDF, how='inner')
     
     # [[[save to csv file]]]
-    fullDF.to_csv(os.path.join(config.basePathS, 'mergedDF.csv'))
+    fullDF.to_csv(os.path.join(config.basePathS, 'fullDF.csv'))
+    
+    # [[[testing plotting]]]
+    fullDF.loc[:, 'DemIsHigher2008'] = (fullDF.loc[:, 'Election2008Dem'] >
+                                        fullDF.loc[:, 'Election2008Rep'])
+#    fullDF.loc[fullDF.loc[:, 'DemIsHigher2008'], 'ShapeColor'] = \
+#        [(0, 0, 1)] * np.sum(fullDF.loc[:, 'DemIsHigher2008'])
+    fullDF.loc[fullDF.loc[:, 'DemIsHigher2008'], ['Red', 'Green', 'Blue']] = \
+        [(0, 0, 1)] * np.sum(fullDF.loc[:, 'DemIsHigher2008'])
+    fullDF.loc[~fullDF.loc[:, 'DemIsHigher2008'], 'ShapeColor'] = \
+        [(1, 0, 0)] * np.sum(~fullDF.loc[:, 'DemIsHigher2008'])
+    fullDF.to_csv(os.path.join(config.basePathS, 'fullDFWithColors.csv'))
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    pdb.set_trace()
+    for shapePatch, shapeColorT in zip(fullDF.loc[:, 'ShapePatches'],
+                                       fullDF.loc[:, 'ShapeColor']):
+        ax.add_collection(PatchCollection(shapePatch, color=tuple(shapeColorT)))
+    ax.set_xlim(np.amin(fullDF.ShapeXMin), np.amax(fullDF.ShapeXMax))
+    ax.set_ylim(np.amin(fullDF.ShapeYMin), np.amax(fullDF.ShapeYMax))
     
   # Transform all of that data into a usable form
   # {{{}}}
@@ -67,6 +94,4 @@ def main():
   # Depending on the correlations you find, plot the most interesting results on a county map of the US
   # {{{}}}
   
-    # [[[obviously temporary]]]
-#    return (unemploymentDF_L, election2012_DF)
-    return (fullDF, unemploymentDF_L, election2008_DF, election2012_DF)
+    return fullDF
